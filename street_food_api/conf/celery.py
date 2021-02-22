64904f9591281fb0fbd00036bb63f98c4e15fb84
Adam Szustak <madasz@onet.eu>
2021-02-22 18:29:16 +0100

    Added async geolocation

--- street_food_api/conf/celery.py
+++ street_food_api/conf/celery.py
@@ -2,18 +2,18 @@ import os
 
 from celery import Celery
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'conf.settings.local')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "conf.settings.local")
 
-app = Celery('street_food_api')
+app = Celery("street_food_api")
 
-app.config_from_object('django.conf:settings', namespace='CELERY')
+app.config_from_object("django.conf:settings", namespace="CELERY")
 
 app.autodiscover_tasks()
 
 
 @app.task(bind=True)
 def debug_task(self):
-    print(f'Request: {self.request!r}')
+    print(f"Request: {self.request!r}")
 
 
 # poetry run celery -A conf worker --loglevel=INFO
--- street_food_api/locations/tasks.py
+++ street_food_api/locations/tasks.py
@@ -1,6 +1,5 @@
-from celery import shared_task
 import requests
-
+from celery import shared_task
 from django.conf import settings
 
 from .models import Location
@@ -9,8 +8,13 @@ from .models import Location
 @shared_task
 def get_geolocation(id):
     location = Location.objects.get(truck_id=id)
-    response = requests.get("http://api.positionstack.com/v1/forward", {"access_key":
-    settings.GEO_KEY, "query": f"{location.zip_code} {location.street}, {location.city}, Poland"})
-    location.latitude = response.json()['data'][0]['latitude']
-    location.longitude = response.json()['data'][0]['longitude']
+    response = requests.get(
+        "http://api.positionstack.com/v1/forward",
+        {
+            "access_key": settings.GEO_KEY,
+            "query": f"{location.zip_code} {location.street}, {location.city}, Poland",
+        },
+    )
+    location.latitude = response.json()["data"][0]["latitude"]
+    location.longitude = response.json()["data"][0]["longitude"]
     location.save()
